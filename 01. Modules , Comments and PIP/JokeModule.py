502ecf27481505038296c69f2a5405afdeaa2d03
Rachit Shrirame <shriramerachit@gmail.com>
2026-05-27 12:17:20 +0530

    Add files via upload

---
+++ 01. Modules , Comments and PIP/JokeModule.py
@@ -0,0 +1,11 @@
+import pyjokes
+
+print("Printing Jokes...")
+# print("this is a comment")
+joke = pyjokes.get_joke()
+print(joke)
+
+
+"""
+Okay this is multiline comment
+"""
---
+++ 01. Modules , Comments and PIP/TextToSpeech.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello im cronex at your service")
+engine.runAndWait()
